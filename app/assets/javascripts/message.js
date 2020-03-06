cf2dd9f236beec755a8bb9fe7cefa1013d41df83
yuku214 <60311206+yuku214@users.noreply.github.com>
2020-03-06 21:02:21 +0900

    change view

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -4,16 +4,17 @@ $(function(){
       var html = `<div class="message" data-message-id="${message.id}">
                   <div class="upper-message">
                     <div class="upper-message__user-name">
-                    ${message.user_name}
+                      ${message.user_name}
                     </div>
                     <div class="upper-message__date">
-                    ${message.created_at}
+                      ${message.created_at}
                     </div>
+                  </div>
                   <div class="lower-message">
-                    <p class="lower-message__content>
+                    <p class="lower-message__content">
                       ${message.content}
                     </p>
-                    <img src="${message.image}" class="lower-message__image>
+                    <img src="${message.image}" class="lower-message__image">
                   </div>
                 </div>`
     } else if (message.content) {
